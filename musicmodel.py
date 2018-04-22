67a90fde5eb6ac8f5b05f3c35fb4c4ece98a9ebf
William Merrill <vikingarnir.will@gmail.com>
2018-04-22 16:46:49 -0400

    sigmoid

--- musicmodel.py
+++ musicmodel.py
@@ -14,8 +14,9 @@ TIME_STEPS = 60
 N_FEATURES = 128
 N_EMBED = 64
 N_HIDDEN = 256
-N_OUTPUT = 349632 # 128 choose 2 + 128 choose 3 + 128
-N_EPOCHS = 100
+# N_OUTPUT = 349632 # 128 choose 2 + 128 choose 3 + 128
+N_OUTPUT = N_FEATURES
+N_EPOCHS = 3 # 100
 BATCH_SIZE = 30
 ETA = .01
 n_lstm_layers = 2
@@ -67,8 +68,8 @@ class MusicGen:
 			X = tf.concat([self.X_hold, self.X_hit], axis=2)
 
 			# Output tensors
-			self.Y_hold = tf.placeholder(tf.int32, shape=[BATCH_SIZE, TIME_STEPS])
-			self.Y_hit = tf.placeholder(tf.int32, shape=[BATCH_SIZE, TIME_STEPS])
+			# self.Y_hold = tf.placeholder(tf.int32, shape=[BATCH_SIZE, TIME_STEPS])
+			# self.Y_hit = tf.placeholder(tf.int32, shape=[BATCH_SIZE, TIME_STEPS])
 
 			hidden_state = tf.zeros(shape=[BATCH_SIZE, N_HIDDEN])
 			current_state = tf.zeros([BATCH_SIZE, N_HIDDEN])
@@ -85,12 +86,13 @@ class MusicGen:
 				y = tf.matmul(h, self.W) + self.b
 				y_hold, y_hit = tf.split(y, [N_OUTPUT, N_OUTPUT], axis=1)
 
-				# t + 1 is right here?
-				self.loss += tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y_hold, labels=self.Y_hold[:,t + 1])
-				self.loss += tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y_hit, labels=self.Y_hit[:,t + 1])
+				# Note: DeepJazz uses softmax over tokens, not multiclass sigmoid
+				self.loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_hold, labels=self.X_hold[:,t + 1]))
+				self.loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_hit, labels=self.X_hit[:,t + 1]))
 				# self.loss += tf.losses.mean_squared_error(self.X[:,t + 1,:], y)
 				# self.loss += tf.cast(tf.count_nonzero(tf.cast(y, tf.int64)), tf.float32) # TODO make this smooth
 
+			self.loss /= t + 1
 			self.train_step = tf.train.AdamOptimizer(ETA).minimize(tf.reduce_sum(self.loss))
 
 		print("Train graph built successfully!")
@@ -109,40 +111,39 @@ class MusicGen:
 			self.current_state = tf.placeholder(tf.float32, shape=[BATCH_SIZE, N_HIDDEN])
 			state = self.hidden_state, self.current_state
 
-			# TODO link this to code above
 			h, state = self.stacked_lstm(x, state)
 			y = tf.matmul(h, self.W) + self.b
 			self.y_hold, self.y_hit = tf.split(y, [N_OUTPUT, N_OUTPUT], axis=1)
-			self.y_hold = tf.nn.softmax(self.y_hold)
-			self.y_hit = tf.nn.softmax(self.y_hit)
+			self.y_hold = tf.round(tf.sigmoid(self.y_hold)) #softmax
+			self.y_hit = tf.round(tf.sigmoid(self.y_hit))
 
 			self.next_hidden_state, self.next_current_state = state
 
 		print("Gen graph build successfully!")
 
-	def train(self, X_hold, X_hit, Y_hold, Y_hit, session):
+	def train(self, X_hold, X_hit, session):
 		for epoch in xrange(N_EPOCHS):
-			X_hold, X_hit, Y_hold, Y_hit = shuffle(X_hold, X_hit, Y_hold, Y_hit)
+			X_hold, X_hit = shuffle(X_hold, X_hit)
 			loss = 0.
 			for i in xrange(0, len(X_hold) - BATCH_SIZE, BATCH_SIZE):
 				batch_loss, _ = session.run([self.loss, self.train_step], feed_dict={
 					self.X_hold: X_hold[i:i + BATCH_SIZE, :, :],
 					self.X_hit: X_hit[i:i + BATCH_SIZE, :, :],
-					self.Y_hold: Y_hold[i:i + BATCH_SIZE, :],
-					self.Y_hit: Y_hit[i:i + BATCH_SIZE, :],
+					# self.Y_hold: Y_hold[i:i + BATCH_SIZE, :],
+					# self.Y_hit: Y_hit[i:i + BATCH_SIZE, :],
 				})
 				loss += batch_loss
 			print("Epoch {} train loss: {}".format(epoch, loss))
 
 	# TODO this function will not "generate" songs.. but that won't be too different
-	def predict(self, x_hold, x_hit, y_hold, y_hit, session, length=3600):
+	def predict(self, x_hold, x_hit, session, length=3600):
 
 		hidden_state = np.zeros(shape=[BATCH_SIZE, N_HIDDEN])
 		current_state = np.zeros(shape=[BATCH_SIZE, N_HIDDEN])
 
 		nodes = [self.y_hold, self.y_hit, self.next_hidden_state, self.next_current_state]
 
-		for i in xrange(X.shape[1]):
+		for i in xrange(x_hold.shape[1]):
 			y_hold, y_hit, hidden_state, current_state = session.run(nodes, feed_dict={
 				self.x_hold: x_hold[:,i,:],
 				self.x_hit: x_hit[:,i,:],
@@ -153,14 +154,14 @@ class MusicGen:
 		pred_hold, pred_hit = [], []
 		for i in xrange(length):
 
-			y_hold = one_to_multihot(y_hold)
-			y_hit = one_to_multihot(y_hit)
+			# y_hold = one_to_multihot(y_hold)
+			# y_hit = one_to_multihot(y_hit)
 			pred_hold.append(y_hold)
 			pred_hit.append(y_hit)
 
 			y_hold, y_hit, hidden_state, current_state = session.run(nodes, feed_dict={
-				self.y_hold: y_hold,
-				self.y_hit: y_hit,
+				self.x_hold: y_hold,
+				self.x_hit: y_hit,
 				self.hidden_state: hidden_state,
 				self.current_state: current_state,
 			})
@@ -184,19 +185,19 @@ if __name__ == "__main__":
 	# # The data should have shape (num_trials, time_steps, num_features)
 	# print("Training data of shape {}".format(data.shape))
 
-	print("Loading data")
+	print("Loading data..")
 	X_hold, X_hit, attr = encode("data/songs/moonlightinvermont.mid", False)
 	
 	# Cut off extra stuff
 	X_hold, X_hit = map(crop_data, [X_hold, X_hit])
 	
 	# Build Y tensors
-	y_hold, y_hit = map(multi_to_onehot, [X_hold, X_hit])
+	# y_hold, y_hit = map(multi_to_onehot, [X_hold, X_hit])
 
 	# Stack by timesteps
-	X_hold, X_hit, y_hold, y_hit = map(stack, [X_hold, X_hit, y_hold, y_hit])
+	X_hold, X_hit = map(stack, [X_hold, X_hit]) # , y_hold, y_hit
 
-	print("X_hold shape: {}, y_hold shape: {}".format(X_hold.shape, y_hold.shape))
+	print("Loaded tensors of shapes {}, {}!".format(X_hold.shape, X_hit.shape))
 
 	gen = MusicGen()
 	gen.add_train_graph()
@@ -207,18 +208,15 @@ if __name__ == "__main__":
 	session.run(tf.global_variables_initializer())
 
 	print("Training..")
-	gen.train(X_hold, X_hit, y_hold, y_hit, session)
+	gen.train(X_hold, X_hit, session)
 	print("Training completed!")
 
 	print("Predicting..")
 	pred_hold, pred_hit = gen.predict(X_hold[:BATCH_SIZE,:N_SEED, :], X_hit[:BATCH_SIZE,:N_SEED, :], session)
-	print("  prediction tensor of shape {}".format(predictions.shape))
+	print("Predicted tensors of shapes {}, {}!".format(pred_hold.shape, pred_hit.shape))
+	
 	print("Encoding MIDI..")
 	pred_hold, pred_hit = pred_hold[0,:,:], pred_hit[0,:,:]
-	# stats(prediction)
-	# prediction = prediction.tolist()
-
-	pattern = decode(pred_hold, pred_hit, attributes)
+	pattern = decode(pred_hold, pred_hit, attr)
 	midi.write_midifile("output.mid", pattern)
-
-	print("Got prediction tensors of shape {}, {}".format(pred_hold.shape, pred_hit.shape))
+	print("MIDI saved!")
