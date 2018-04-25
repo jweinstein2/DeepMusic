5174365751d5535e4bf4d7bf8b73103d1c8d7fc4
Jared Weinstein <jtw37@zoo.cs.yale.edu>
2018-04-25 14:29:08 -0400

    onehot encoding functions

--- data/convert.py
+++ data/convert.py
@@ -4,12 +4,11 @@ import numpy as np
 import midi
 from itertools import combinations
 from sklearn import preprocessing
+import itertools
 
 import matplotlib.pyplot as plt
 
 import pdb
-
-N_NOTES_MAX = 3
 
 def _compress(grid):
     reduced_data = []
@@ -17,18 +16,23 @@ def _compress(grid):
         reduced_data.append(np.max(grid[:,i::12], axis=1))
     return np.stack(reduced_data, axis=1)
 
-def one_to_multihot(array, n=N_NOTES_MAX, l=128):
-    combos = []
-    for i in range(n + 1):
-        combos += combinations(range(l), i)
+def multihot(data, le):
+    str_multi = le.inverse_transform(data)
+    lst_multi = map(lambda x: np.fromstring(x[1:-1], dtype=int, sep=' '), str_multi)
+    lst_multi = np.array(lst_multi)
+    return lst_multi
 
-    mlb = preprocessing.MultiLabelBinarizer()
-    labels = mlb.fit_transform(combos)
+def onehot(arr1, arr2):
+    le = preprocessing.LabelEncoder()
+    arr1_str = [np.array_str(i).replace('\n', '') for i in arr1]
+    arr2_str = [np.array_str(i).replace('\n', '') for i in arr2]
 
-    return np.apply_along_axis(lambda x: labels[x], axis=0, arr=array)
+    le.fit(arr1_str + arr2_str)
 
+    arr1_oh = le.transform(arr1_str)
+    arr2_oh = le.transform(arr2_str)
+    return arr1_oh, arr2_oh, le
 
-def multi_to_onehot(array, n=N_NOTES_MAX, l=128):
     combos = []
     for i in range(n + 1):
         combos += combinations(range(l), i)
@@ -99,7 +103,7 @@ def encode(midifile, compress=False):
     hold = np.clip(grid, a_min=0, a_max=1)
     previous = np.roll(grid, 1, axis=0)
     previous[0,:] = 0
-    change = (previous != grid).astype(np.int32)
+    change = (previous != grid).astype(np.int16)
     hit = np.multiply(change, hold)
 
     print("encoded shape {}".format(np.asarray(grid).shape))
@@ -182,12 +186,13 @@ if __name__ == "__main__":
     for filename in os.listdir(songs_dir):
         if filename.endswith('.mid'):
             hold, hit, a = encode(songs_dir + filename, False)
-            # oh_hold = multi_to_onehot(hold)
-            # oh_hit = multi_to_onehot(hit)
-            print("onehot shape {}".format(hold.shape))
-            # mh_hold = one_to_multihot(oh_hold)
-            # mh_hit = one_to_multihot(oh_hit)
-            pattern = decode(hold, hit, a)
+
+            oh_hold, oh_hit, le = onehot(hold, hit)
+            print("onehot shape {}".format(oh_hold.shape))
+            mh_hold = multihot(oh_hold, le)
+            mh_hit = multihot(oh_hit, le)
+
+            pattern = decode(mh_hold, mh_hit, a)
             midi.write_midifile(filename.replace(".mid", "_sample.mid"), pattern)
             # songMatrix = midi_to_matrix.midiToNoteStateMatrix(songs_dir + filename)
             # midi_to_matrix.noteStateMatrixToMidi(songMatrix, name="test")
